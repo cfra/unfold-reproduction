6854cc96c55dec0f62aee2a82c7f0924287f8411
Christian Franke <nobody@nowhere.ws>
2026-02-19 12:38:35 +0100

    Reproduce issue with permission check for initial migration

--- README.md
+++ README.md
@@ -7,8 +7,24 @@ To set up and run, use:
 ```console
 uv sync
 uv run ./manage.py migrate
-uv run ./manage.py createsuperuser
-uv run ./manage.py runserver
 ```
 
-Then navigate to <http://127.0.0.1:8000/admin/app/demomodel/add/>
+Migration fails because of a check introduced in commit:
+
+```
+commit f728bd6b89950b7fa64fcb1580137de9602ec260
+Author: Lukas Vinclav <lukasvinclav@gmail.com>
+Date:   Tue May 20 14:57:03 2025 +0200
+
+    feat: allow django permissions in actions (#1230)
+```
+
+The check is run before migrations are run - it cannot expected the permissions
+table to exist when we run migrate for the very first time.
+
+In our deployements we also run the following prior to running migrate, for this
+I also belive the check to be problematic if database has not been initialized yet:
+
+```
+./manage.py check --deploy --fail-level WARNING
+```
--- app/admin.py
+++ app/admin.py
@@ -1,47 +1,18 @@
 from django.contrib import admin
 from unfold.admin import ModelAdmin
+from unfold.decorators import action
 
 from .models import DemoModel
 
 
 @admin.register(DemoModel)
 class DemoModelAdminClass(ModelAdmin):
-    fieldsets = [
-        (
-            None,
-            {
-                "fields": [
-                    "field_a",
-                    "field_b",
-                    "field_c",
-                    "field_d",
-                    "field_e",
-                ]
-            }
-        ),
-        (
-            "Fieldset 1",
-            {
-                "classes": ["collapse"],
-                "fields": [
-                    "field_f",
-                    "field_g",
-                    "field_h",
-                    "field_i",
-                    "field_j",
-                ]
-            }
-        ),
-        (
-            "Fieldset 2",
-            {
-                "classes": ["collapse"],
-                "fields": [
-                    "field_k",
-                    "field_l",
-                    "field_m",
-                    "field_n",
-                ]
-            }
-        ),
-    ]
+    actions_detail = ["example_action"]
+
+    @action(
+        description="Example Action",
+        permissions=["app.add_demomodel"],
+        url_path="example"
+    )
+    def example_action(self, request, object_id):
+        raise NotImplementedError
